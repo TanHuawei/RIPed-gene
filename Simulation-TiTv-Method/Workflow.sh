b7dbd3568306474bdd842c0060cb1f5c1783c3d9
TanHuawei <90034840+TanHuawei@users.noreply.github.com>
2025-06-03 19:33:25 +0800

    Create Workflow.sh

---
+++ Simulation-TiTv-Method/Workflow.sh
@@ -0,0 +1,28 @@
+
+# Simulating introduce of SNPs, Run several times
+
+# 0.1% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 0.1 Each{} SimuOutDir
+
+# 0.5% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 0.5 Each{} SimuOutDir
+
+# 1% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 1 Each{} SimuOutDir
+
+# 2% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 2 Each{} SimuOutDir
+
+# 5% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 5 Each{} SimuOutDir
+
+# 10% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 10 Each{} SimuOutDir
+
+# 15% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 15 Each{} SimuOutDir
+
+# 20% sequence nucleotide divergence, 3 replicates
+seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 20 Each{} SimuOutDir
+
+
