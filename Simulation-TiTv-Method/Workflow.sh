8b9d4dddce5ea58c9207c501e8bcde8ed5739521
TanHuawei <90034840+TanHuawei@users.noreply.github.com>
2025-06-04 18:47:36 +0800

    Update Workflow.sh

--- Simulation-TiTv-Method/Workflow.sh
+++ Simulation-TiTv-Method/Workflow.sh
@@ -3,28 +3,28 @@
 cd ~/Simulation-TiTv-Method/
 
 # 0.1% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 0.1 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 0.1 Each{} SimuOutDir
 
 # 0.5% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 0.5 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 0.5 Each{} SimuOutDir
 
 # 1% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 1 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 1 Each{} SimuOutDir
 
 # 2% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 2 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 2 Each{} SimuOutDir
 
 # 5% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 5 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 5 Each{} SimuOutDir
 
 # 10% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 10 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 10 Each{} SimuOutDir
 
 # 15% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 15 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 15 Each{} SimuOutDir
 
 # 20% sequence nucleotide divergence, 3 replicates
-seq 1 3 | parallel -j 10 perl Simulation-V1.pl IntroSNP SorMacSN1693 sgl3 20 Each{} SimuOutDir
+seq 1 3 | parallel -j 10 perl Simulation.pl IntroSNP SorMacSN1693 sgl3 20 Each{} SimuOutDir
 
 ############################
 # Stats of simulated SNPs
